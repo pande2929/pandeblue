286274380443d4e7d7ef0923ce640e7128fc5377
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-06-16 21:19:50 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y mangohud
+dnf5 install -y btop
+dnf5 remove -y firefox firefox-langpacks
 
 # Use a COPR Example:
 #
