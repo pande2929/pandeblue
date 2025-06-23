c5c08ccd965f084b4ea2d7d8987cd6530b2f4737
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-06-22 19:50:54 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y mangohud
 dnf5 install -y btop
+dnf5 install -y nvim
 dnf5 remove -y firefox firefox-langpacks
 
 # Use a COPR Example:
