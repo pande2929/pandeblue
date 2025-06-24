7244c3eae5c43eed3506676ce06d48e2e63aea44
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-06-24 08:23:07 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,6 @@ dnf5 install -y btop
 dnf5 install -y nvim
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
-dnf5 install -y @xfce-desktop-environment
 
 # Use a COPR Example:
 #
