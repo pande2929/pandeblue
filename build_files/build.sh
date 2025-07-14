8298125040f387d4953a6f6bdc7da2b0b2b41a4b
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-07-14 09:02:18 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,12 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y mangohud
-dnf5 install -y btop
-dnf5 install -y nvim
-dnf5 install -y @mate-desktop
-dnf5 remove -y firefox firefox-langpacks
-dnf5 remove -y nvtop htop
+#dnf5 install -y mangohud
+#dnf5 install -y btop
+#dnf5 install -y nvim
+#dnf5 install -y @mate-desktop
+#dnf5 remove -y firefox firefox-langpacks
+#dnf5 remove -y nvtop htop
 
 # Use a COPR Example:
 #
