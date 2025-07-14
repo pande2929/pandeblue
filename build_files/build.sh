c5cc51a1ae900f67af811cb53abd163712d7f270
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-07-14 09:39:26 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-#dnf5 install -y mangohud
-#dnf5 install -y btop
-#dnf5 install -y nvim
-dnf5 install -y @mate-desktop
+dnf5 install -y mangohud
+dnf5 install -y btop
+dnf5 install -y nvim
+#dnf5 install -y @mate-desktop
 #dnf5 remove -y firefox firefox-langpacks
 #dnf5 remove -y nvtop htop
 
