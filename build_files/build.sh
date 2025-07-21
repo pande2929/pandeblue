5dc70ae3662c31f9f70421cc98d2391b1bbef23e
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-07-21 10:27:01 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 install -y mangohud
 dnf5 install -y btop
 dnf5 install -y nvim
 dnf5 install -y gdm
-dnf5 install -y @mate-desktop
+#dnf5 install -y @mate-desktop
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
 
