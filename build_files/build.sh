b36457da87acf9035ececac356d731930bc85c6c
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-07-21 10:41:13 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,7 +16,7 @@ dnf5 install -y nvim
 dnf5 install -y gdm
 #dnf5 install -y @mate-desktop
 dnf5 install -y @xfce-desktop-environment
-dnf5 install labwc wlroots
+dnf5 install -y labwc wlroots
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
 
