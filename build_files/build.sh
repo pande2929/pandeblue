54bb2c1f9fe800a14b19ce84db05d4a4286a8bae
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-07-25 13:05:09 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ dnf5 install -y hyfetch
 dnf5 install -y tuigreet
 dnf5 install -y sway
 dnf5 install -y alacritty swaylock swayidle swaybg thunar waybar rofi-wayland pavucontrol
-#dnf5 install -y @kde-desktop-environment
+dnf5 install -y @kde-desktop-environment
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
 
@@ -33,4 +33,5 @@ dnf5 remove -y nvtop htop
 
 #systemctl disable lightdm
 #systemctl enable gdm
-systemctl enable greetd
+#systemctl enable greetd
+systemctl enable sddm
