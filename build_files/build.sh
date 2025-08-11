7aa5e4ce4993464fc6515df831cf92c0b88b0a63
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-08-11 07:49:02 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,16 +18,15 @@ dnf5 install -y btop
 dnf5 install -y nvim
 dnf5 install -y hyfetch
 dnf5 install -y tuigreet
-dnf5 install -y sway
-dnf5 install -y alacritty swaylock swayidle swaybg thunar waybar rofi-wayland pavucontrol fontawesome-fonts-all
+#dnf5 install -y sway alacritty swaylock swayidle swaybg thunar waybar rofi-wayland pavucontrol fontawesome-fonts-all
 #dnf5 install -y @kde-desktop-environment
-#dnf5 install -y @gnome-desktop
+dnf5 install -y @gnome-desktop
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
-dnf5 remove -y foot
+#dnf5 remove -y foot
 
 # Allow sway to work with Nvidia proprietary drivers
-sed -i 's/Exec=sway/Exec=sway --unsupported-gpu/' /usr/share/wayland-sessions/sway.desktop
+#sed -i 's/Exec=sway/Exec=sway --unsupported-gpu/' /usr/share/wayland-sessions/sway.desktop
 
 # Use a COPR Example:
 #
@@ -38,5 +37,5 @@ sed -i 's/Exec=sway/Exec=sway --unsupported-gpu/' /usr/share/wayland-sessions/sw
 
 #systemctl disable lightdm
 #systemctl enable gdm
-systemctl enable greetd
-#systemctl enable sddm
+#systemctl enable greetd
+systemctl enable sddm
