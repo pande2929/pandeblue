03e4d1de15a87d0ba38902384d9ea7f1240d7088
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2025-12-27 23:24:03 -0800

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,6 +21,7 @@ dnf5 install -y hyfetch
 #dnf5 install -y sway alacritty swaylock swayidle swaybg thunar waybar rofi-wayland pavucontrol fontawesome-fonts-all
 #dnf5 install -y @kde-desktop-environment
 #dnf5 install -y @gnome-desktop
+dnf5 install -y @xfce-desktop-environment
 dnf5 remove -y firefox firefox-langpacks
 dnf5 remove -y nvtop htop
 #dnf5 remove -y foot
